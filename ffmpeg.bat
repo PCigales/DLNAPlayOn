3e02c4f8c9b043f914aa294858b4a3afe636010b
PCigales <58741513+PCigales@users.noreply.github.com>
2020-11-15 23:06:30 +0100

    Fix bug introduced with user agent

--- ffmpeg.bat
+++ ffmpeg.bat
@@ -1,4 +1,5 @@
-@set "ffmpeg_path="C:\Program Files\FFmpeg\bin\ffmpeg.exe""
+@set "ffmpeg_path="C:\Program Files\FFmpeg\Media Autobuild Suite\bin\ffmpeg.exe""
+@set "ffmpeg_user_agent="Lavf/ Mozilla/ AppleWebKit/ Chrome/""
 
 @setlocal enabledelayedexpansion
 
@@ -6,19 +7,24 @@
 @if "!mediabuilder_start!" NEQ "" (@set "ffmpeg_st-=-ss 0") else (@set "ffmpeg_st-= ")
 
 @if "!mediabuilder_vid!" == "" (
-@set "ffmpeg_vid= "
-@set "ffmpeg_aud= "
+ @set "ffmpeg_vid= "
+ @set "ffmpeg_aud= "
 ) else (
-@set "ffmpeg_vid=%ffmpeg_st% -i !mediabuilder_vid!"
-@if "!mediabuilder_aud!" == "" (@set "ffmpeg_aud=-map 0") else (@set "ffmpeg_aud=-thread_queue_size 300000 %ffmpeg_st% -i !mediabuilder_aud! -map 0:v -map 1:a")
+ @if "!mediabuilder_vid:://=!" == "!mediabuilder_vid!" (@set "ffmpeg_vid=%ffmpeg_st% -i !mediabuilder_vid!") else (@set "ffmpeg_vid=%ffmpeg_st% -user_agent !ffmpeg_user_agent! -i !mediabuilder_vid!")
+ @if "!mediabuilder_aud!" == "" (
+  @set "ffmpeg_aud=-map 0"
+ ) else (
+  @if "!mediabuilder_aud:://=!" == "!mediabuilder_aud!" (@set "ffmpeg_aud=-thread_queue_size 300000 %ffmpeg_st% -i !mediabuilder_aud! -map 0:v -map 1:a") else (@set "ffmpeg_aud=-thread_queue_size 300000 %ffmpeg_st% -user_agent !ffmpeg_user_agent! -i !mediabuilder_aud! -map 0:v -map 1:a")
+ )
 )
 
 @if "!mediabuilder_sub!" == "" (
-@set "ffmpeg_sub= "
-@set "ffmpeg_lang= ") else (
-@if "!mediabuilder_subcharenc!" == "" (@set "ffmpeg_subcharenc= ") else (@set "ffmpeg_subcharenc=-sub_charenc cp1252")
-@set "ffmpeg_sub=%ffmpeg_st% !ffmpeg_subcharenc! -i !mediabuilder_sub! %ffmpeg_st-%"
-@if "!mediabuilder_lang!" == "" (@set "ffmpeg_lang=-map 0:s:0") else (@set "ffmpeg_lang=-map 0:s:m:language:!mediabuilder_lang:,=? -map 0:s:m:language:!?")
+ @set "ffmpeg_sub= "
+ @set "ffmpeg_lang= "
+) else (
+ @if "!mediabuilder_subcharenc!" == "" (@set "ffmpeg_subcharenc= ") else (@set "ffmpeg_subcharenc=-sub_charenc cp1252")
+ @if "!mediabuilder_sub:://=!" == "!mediabuilder_sub!" (@set "ffmpeg_sub=%ffmpeg_st% !ffmpeg_subcharenc! -i !mediabuilder_sub! %ffmpeg_st-%") else (@set "ffmpeg_sub=%ffmpeg_st% !ffmpeg_subcharenc! -user_agent !ffmpeg_user_agent! -i !mediabuilder_sub! %ffmpeg_st-%")
+ @if "!mediabuilder_lang!" == "" (@set "ffmpeg_lang=-map 0:s:0") else (@set "ffmpeg_lang=-map 0:s:m:language:!mediabuilder_lang:,=? -map 0:s:m:language:!?")
 )
 
 @set "ffmpeg_par=-c:v copy -c:a copy -sn -dn -y -f !mediabuilder_mux!"
@@ -41,7 +47,7 @@
 
 @if /I "!mediabuilder_mux!" == "SRT--" @exit 1
 
-@%ffmpeg_path% -user-agent "Lavf/ Mozilla/ AppleWebKit/ Chrome/" !ffmpeg_vid! !ffmpeg_aud! !ffmpeg_sub! %ffmpeg_par% -listen 1 !mediabuilder_address!
+@%ffmpeg_path% !ffmpeg_vid! !ffmpeg_aud! !ffmpeg_sub! %ffmpeg_par% -listen 1 !mediabuilder_address!
 
 @endlocal
 
